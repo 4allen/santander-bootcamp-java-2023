ad513ed88b395b2afb30987d4336537f7683999f
hellen atanasio <l.atanasio@hotmail.com>
2023-10-20 22:58:32 -0300

    Hotfix "precision" e "scale"

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -12,9 +12,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "limite adiciolar", scale = 13, precision = 2)
+    @Column(name = "limite adiciolar", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
